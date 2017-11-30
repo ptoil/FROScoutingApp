3f70d8c27763786b396c6d2be5678bdbbe5ec389
ptoil <suhailmallick@gmail.com>
2017-11-29 19:53:26 -0500

    Snackbar work

--- app/src/main/java/org/team5401/froscoutingapp/CreateScoutingInput.java
+++ app/src/main/java/org/team5401/froscoutingapp/CreateScoutingInput.java
@@ -3,6 +3,7 @@ package org.team5401.froscoutingapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -100,5 +101,6 @@ public class CreateScoutingInput extends AppCompatActivity {
             editor.putString(valueOf(i), json);
         }
         editor.apply();
+        Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Layout Applied", Snackbar.LENGTH_SHORT).show();
     }
 }
