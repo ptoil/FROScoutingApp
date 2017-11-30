c7009a5fc9c8b4168dc32c5b301b7f44472ca5af
ptoil <suhailmallick@gmail.com>
2017-11-29 19:19:35 -0500

    It worked! I'm finally successfully committing and pushing changes with my account

--- app/src/main/java/org/team5401/froscoutingapp/Scouting.java
+++ app/src/main/java/org/team5401/froscoutingapp/Scouting.java
@@ -41,7 +41,7 @@ public class Scouting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scouting);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //temporary for testing committing and pushing
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         viewsInputDataArray = new ArrayList<InputData>();
         viewsArray = new ArrayList<DataHolder>();
